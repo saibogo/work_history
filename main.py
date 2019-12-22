2ff6cad125ee2f55d0571dfecf92a3fbc2783275
saibogo <gleykh@gmail.com>
2019-12-22 20:30:58 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from wh_app.simple_gui import tkinter_gui as gui
 from wh_app.sql_operations import view_operation
 from wh_app.postgresql.database import Database
-
+import sys
+from wh_app.supporting import stop_start_web
 
 with Database() as base:
     connection, cursor = base
@@ -11,4 +12,16 @@ with Database() as base:
 
     connection.commit()
 
-gui.main_window()
+if len(sys.argv) > 1:
+    command = sys.argv[1]
+    if command == '--startserver':
+        stop_start_web.start_server()
+    elif command == '--stopserver':
+        stop_start_web.stop_server()
+    elif command == '--help':
+        print('--help, --startserver, --stopserver')
+    else:
+        print('Error command. Use --help for more information')
+
+else:
+    gui.main_window()
