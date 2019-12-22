de8bc81cd0d6d34a616a22144eacd270123a1097
saibogo <gleykh@gmail.com>
2019-12-22 20:34:47 +0500

    Add files via upload

---
+++ wh_app/supporting/stop_start_web.py
@@ -0,0 +1,33 @@
+import wh_app.config_and_backup.config as config
+import threading
+import requests
+import psutil
+
+import wh_app.web.server as webserver
+from wh_app.supporting import functions
+
+functions.info_string(__name__)
+
+
+def start_server():
+    try:
+        _ = requests.get("http://" + config.ip_address + ":" + config.port + '/add-work')
+        print("Веб-сервер уже запущен")
+    except:
+        print("Запускаем веб-сервер")
+        threading.Thread(target=webserver.start_server).start()
+
+
+def stop_server():
+    try:
+        _ = requests.get("http://" + config.ip_address + ":" + config.port + '/add-work')
+        for process in psutil.process_iter():
+            data = process.as_dict(attrs=['cmdline', 'pid'])
+            for elem in data['cmdline']:
+                if 'work_history' in elem:
+                    print(data)
+                    print("процесс найден PID=" + str(data['pid']))
+                    process.kill()
+                    print("Веб-сервер остановлен")
+    except:
+        print("Веб-сервер не запущен")
