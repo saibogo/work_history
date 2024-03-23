e539803e5d83a6501cfdad68b9f1c5a3ea861cd4
saibogo <gleykh@gmail.com>
2024-03-23 15:38:42 +0500

    Remove Qt

--- wh_app/telegram_bot/write_bot_access.py
+++
@@ -1,5 +0,0 @@
-'''{key = telegram_id: value = workhistory_id}'''
-write_access_dict = {145479584: '1',
-                     5527149033: '9',
-                     1137815006: '8',
-                     2143859072: '5'}
