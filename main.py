d6e29028a980e13bf2902038dc3fec4f837b396e
saibogo <gleykh@gmail.com>
2025-05-15 21:35:34 +0500

    Refactoring command line interface

--- main.py
+++ main.py
@@ -2,29 +2,30 @@
 
 import sys
 
-from wh_app.sql_operations import view_operation, sql_functions_operations, sql_procedures_operations
-from wh_app.sql_operations.select_operations.select_operations import get_database_version as dbversion
-from wh_app.postgresql.database import Database
-from wh_app.sql_operations.call_operations import find_all_table_to_vacuum
-from wh_app.supporting.cli import COMMANDS, COMMANDS_EXT, print_help
-from wh_app.supporting.backup_operations import delete_old_backups
 
-with Database() as base:
-    CONNECTION, CURSOR = base
-    print("Connect to Database {0}".format(dbversion(CURSOR)[0][0]))
-    VIEW_LIST = view_operation.all_view_list()
-    for view in VIEW_LIST:
-        view(CURSOR)
+def __start() -> None:
+    """Start with SQL server"""
+    from wh_app.sql_operations import view_operation, sql_functions_operations, sql_procedures_operations
+    from wh_app.sql_operations.select_operations.select_operations import get_database_version as dbversion
+    from wh_app.postgresql.database import Database
+    from wh_app.sql_operations.call_operations import find_all_table_to_vacuum
 
-    SQL_FUNCTIONS_LIST = sql_functions_operations.all_sql_functions_list()
-    for sql_funct in SQL_FUNCTIONS_LIST:
-        sql_funct(CURSOR)
+    with Database() as base:
+        CONNECTION, CURSOR = base
+        print("Connect to Database {0}".format(dbversion(CURSOR)[0][0]))
+        VIEW_LIST = view_operation.all_view_list()
+        for view in VIEW_LIST:
+            view(CURSOR)
 
-    SQL_PROCEDURES_LIST = sql_procedures_operations.all_sql_procedures_list()
-    for sql_proc in SQL_PROCEDURES_LIST:
-        sql_proc(CURSOR)
+        SQL_FUNCTIONS_LIST = sql_functions_operations.all_sql_functions_list()
+        for sql_funct in SQL_FUNCTIONS_LIST:
+            sql_funct(CURSOR)
 
-    CONNECTION.commit()
+        SQL_PROCEDURES_LIST = sql_procedures_operations.all_sql_procedures_list()
+        for sql_proc in SQL_PROCEDURES_LIST:
+            sql_proc(CURSOR)
+
+        CONNECTION.commit()
 
     with Database() as base:
         connection, cursor = base
@@ -35,14 +36,26 @@ with Database() as base:
 
 if len(sys.argv) > 1:
     COMMAND = sys.argv[1]
-    if COMMAND in COMMANDS:
-        COMMANDS[COMMAND]()
-    elif COMMAND in COMMANDS_EXT and len(sys.argv) > 2:
-        COMMANDS_EXT[COMMAND](sys.argv[2])
+    from wh_app.supporting.cli import no_sql_commands
+    if COMMAND in no_sql_commands():
+        no_sql_commands()[COMMAND]()
+    else:
+        from wh_app.supporting.cli import commands_with_sql
+        cws = commands_with_sql()
+        if COMMAND in cws:
+            __start()
+            cws[COMMAND]()
+elif len(sys.argv) > 2:
+    COMMAND = sys.argv[1]
+    from wh_app.supporting.cli import commands_ext
+    if COMMAND in commands_ext():
+        __start()
+        commands_ext()[COMMAND](sys.argv[2])
     else:
         print('Error command. Use --help for more information')
 
 else:
+    from wh_app.supporting.cli import print_help
     print('Not corrected start argument!')
     print_help()
 
--- wh_app/supporting/cli.py
+++ wh_app/supporting/cli.py
@@ -1,20 +1,17 @@
 """This module implements the command line interface"""
 
-from wh_app.supporting import stop_start_web
-from wh_app.supporting import backup_operations as bcp_oper
-from wh_app.supporting import functions
-from wh_app.supporting import delete_views_and_tables as cleardb
-from wh_app.supporting import users_operation
-from wh_app.supporting import backup_operations
 
-
-functions.info_string(__name__)
+def print_version() -> None:
+    """Print program`s version"""
+    from wh_app.supporting.metadata import __version__ as vers
+    print(vers)
 
 
 def print_help() -> None:
     """Implement --help argument in command line interface"""
     print('python3 /path/to/main.py [command] [argument]')
     print('-h, --help : View this help')
+    print('-v, --version: Print current program version')
     print('--startserver : Start WebServer in not running')
     print('--stopserver ; Stop WebServer in running')
     print('--allstart : Start server and autosave procedure')
@@ -29,10 +26,19 @@ def print_help() -> None:
     print('if not arguments - Break Program and print HELP section')
 
 
-COMMANDS = {'--startserver': stop_start_web.start_server,
+def no_sql_commands() -> dict:
+    """Return all commands without SQL"""
+    return {'--help': print_help, '-h': print_help, '-v': print_version, '--version': print_version}
+
+
+def commands_with_sql() -> dict:
+    """Return all single commands with using SQL"""
+    from wh_app.supporting import stop_start_web
+    from wh_app.supporting import delete_views_and_tables as cleardb
+    from wh_app.supporting import users_operation
+    from wh_app.supporting import backup_operations
+    return {'--startserver': stop_start_web.start_server,
             '--stopserver': stop_start_web.stop_server,
-            '--help': print_help,
-            '-h': print_help,
             '--statusserver': lambda: print("Веб-сервер работает" if stop_start_web.status_server()
                                             else "Веб-сервер не запущен"),
             '--cleardb': cleardb.drop_all_data,
@@ -41,6 +47,12 @@ COMMANDS = {'--startserver': stop_start_web.start_server,
             '--createscheme' : backup_operations.create_empty,
             '--allstart': stop_start_web.all_start}
 
-COMMANDS_EXT = {'--savedb': bcp_oper.create_dump,
-                '--updatepassword': users_operation.update_password,
-                '--saystop': stop_start_web.say_stop}
+
+def commands_ext() -> dict:
+    """Return all command with arguments and using SQL"""
+    from wh_app.supporting import stop_start_web
+    from wh_app.supporting import backup_operations as bcp_oper
+    from wh_app.supporting import users_operation
+    return {'--savedb': bcp_oper.create_dump,
+            '--updatepassword': users_operation.update_password,
+            '--saystop': stop_start_web.say_stop}
